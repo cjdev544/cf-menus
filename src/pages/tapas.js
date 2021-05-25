2f74c83f464a237977afe7ce8da3e769fdafbdec
CJDev <cjdev544@gmail.com>
2021-05-25 11:32:10 -0400

    Add bebidas and tapas menu 25/05/21

--- src/pages/tapas.js
+++ src/pages/tapas.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import AppLayout from '../components/AppLayout'
 import GoBack from '../components/GoBack/GoBack'
 
 const Tapas = () => {
   return (
     <AppLayout>
-      <div>
-        <h1>Aquí se pondra el menú de tapas</h1>
+      <div className="menu-page">
+        <StaticImage
+          className="menu-page__image"
+          src="../images/tapas-bebidas.jpg"
+          alt="página 1 del menu de tapas"
+          placeholder="tracedSVG"
+          layout="constrained"
+          objectFit="cover"
+          width={1000}
+        />
+        <StaticImage
+          className="menu-page__image"
+          alt="página 2 del menu de tapas"
+          src="../images/tapas.jpg"
+          placeholder="tracedSVG"
+          layout="constrained"
+          objectFit="cover"
+          width={1000}
+        />
       </div>
       <GoBack />
     </AppLayout>
